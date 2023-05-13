2b82d98b110cdf491278811981f3418d00180948
Carlos Reyes <chentito002@gmail.com>
2023-05-12 23:38:13 -0600

    api and api resourc

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DragonTreasureRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
+#[ApiResource]
 class DragonTreasure
 {
     #[ORM\Id]
